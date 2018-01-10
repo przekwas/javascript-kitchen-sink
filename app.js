d1fbeffe09ce08df3f66b0d1fa9bab50dfe7ac3c
Luke Przekwas <galactica@Lukes-MacBook-Pro.local>
2018-01-10 12:46:38 -0600

    code to alert for letters after L in name

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 //global variables
 let myName = "Luke Przekwas";
 const totalUnitedStates = 50;
+let sum = 5 + 4;
+
+/*code to display an alert if name starts with a letter after L
+checks if the ascii code at the first letter position is greater than 76 (ascii for capital L)*/
+if (myName.charCodeAt(0) > 76) {
+    alert("Back of the line!");
+} else {
+    alert("Next!");
+}
+
 
